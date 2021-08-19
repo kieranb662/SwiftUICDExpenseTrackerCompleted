22f818bc2969fa91d65f8beada9da09df2f5e1cb
Kieran Brown <kb6627500@gmail.com>
2021-08-19 13:48:43 -0400

    added new file for monthly summary tab

---
+++ ExpenseTracker/Views/Common Views/MonthlySummary/MonthlySummaryTab.swift
@@ -0,0 +1,27 @@
+// Swift toolchain version 5.0
+// Running macOS version 12.0
+// Created on 8/19/21.
+//
+// Author: Kieran Brown
+//
+
+import SwiftUI
+
+// Create a new view for “monthly summary”, the user can access the view from the bottom submenu
+// (add a third icon to the submenu).
+
+// On the view, all twelve months should display and the User can
+// select one or multiple months from the screen and the logs for the selected months will be
+// displayed in order of date.
+
+struct MonthlySummaryTab: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MonthlySummaryTab_Previews: PreviewProvider {
+    static var previews: some View {
+        MonthlySummaryTab()
+    }
+}
